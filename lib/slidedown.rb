31c915f8fdc3225e41683359256e9bbb2fff354e
Pat Nakajima <patnakajima@gmail.com>
2009-03-10 13:46:57 -0400

    Code is better when it's not 4am

--- lib/slidedown.rb
+++ lib/slidedown.rb
@@ -4,28 +4,52 @@ require 'rdiscount'
 require 'albino'
 require 'erb'
 
-raw = File.read(File.join(Dir.pwd, ARGV.first))
-
-@classes = []
-raw.gsub!(/^!SLIDE\s*([a-z\s]*)$/) do |klass|
-  @classes << klass.to_s.chomp
-  "!SLIDE"
-end
-
-@slides =  raw.split(/^!SLIDE\s*([a-z\s]*)$/) \
-  .reject { |line| line.empty? } \
-  .map { |slide| RDiscount.new(slide).to_html } \
-  .map do |slide| 
+class SlideDown
+  attr_reader :classes
+  
+  def initialize(args)
+    @raw = File.read(File.join(Dir.pwd, *args))
+    extract_classes!
+  end
+  
+  def slides
+    @slides ||= lines \
+      .reject { |line| line.empty? } \
+      .map { |slide| RDiscount.new(slide).to_html } \
+      .map { |slide| highlight(slide) }
+  end
+  
+  def read(path)
+    File.read(File.dirname(__FILE__) + '/../templates/%s' % path)
+  end
+  
+  def render
+    template = File.read(File.dirname(__FILE__) + '/../templates/template.erb')
+    puts ERB.new(template).result(binding)
+  end
+  
+  private
+  
+  def highlight(slide)
     doc = Nokogiri::HTML(slide)
-    
+
     doc.search('pre code').each do |snippet|
       node = Nokogiri::HTML(Albino.new(snippet.text, :ruby).to_s).at('div')
       snippet.replace(node)
     end
-    
+
     doc.at('body').children.to_s rescue doc.to_s
   end
-
-template = File.read(File.dirname(__FILE__) + '/../templates/template.erb')
-
-puts ERB.new(template).result(binding)
+  
+  def lines
+    @lines ||= @raw.split(/^!SLIDE\s*([a-z\s]*)$/)
+  end
+  
+  def extract_classes!
+    @classes = []
+    @raw.gsub!(/^!SLIDE\s*([a-z\s]*)$/) do |klass|
+      @classes << klass.to_s.chomp.gsub('!SLIDE', '')
+      "!SLIDE"
+    end ; @classes
+  end
+end
